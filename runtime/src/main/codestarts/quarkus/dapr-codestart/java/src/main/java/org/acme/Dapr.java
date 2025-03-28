5d07092adc6a384f29cb9fd25695577ce3d8e247
jghagemann <hagejotage@hotmail.com>
2025-03-27 23:17:00 -0300

    Correcting codestart Dapr class

--- runtime/src/main/codestarts/quarkus/dapr-codestart/java/src/main/java/org/acme/Dapr.java
+++ runtime/src/main/codestarts/quarkus/dapr-codestart/java/src/main/java/org/acme/Dapr.java
@@ -1,11 +1,13 @@
 package org.acme;
 
-import jakarta.enterprise.context.ApplicationScoped;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain(name = "quarkus-dapr")
 public class Dapr implements QuarkusApplication {
 
-    public String hello() {
-        return "My Example Hello Quarkus-Dapr Codestart";
+    @Override public int run(String... args) throws Exception {
+        System.out.println("Hello quarkus-dapr codestart");
+        return 0;
     }
 }
