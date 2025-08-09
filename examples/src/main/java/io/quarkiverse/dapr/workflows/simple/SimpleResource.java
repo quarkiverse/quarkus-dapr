bedab83993737a2e12f21779e57d3bc514d7f44a
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-08-09 00:59:52 -0300

    Add a verifier endpoint

--- examples/src/main/java/io/quarkiverse/dapr/workflows/simple/SimpleResource.java
+++ examples/src/main/java/io/quarkiverse/dapr/workflows/simple/SimpleResource.java
@@ -1,19 +1,21 @@
 package io.quarkiverse.dapr.workflows.simple;
 
+import io.dapr.workflows.client.DaprWorkflowClient;
+import io.dapr.workflows.client.WorkflowInstanceStatus;
+import io.dapr.workflows.runtime.WorkflowRuntimeBuilder;
+import io.dapr.workflows.runtime.WorkflowRuntimeStatus;
+import io.quarkus.logging.Log;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-import io.dapr.workflows.client.DaprWorkflowClient;
-import io.dapr.workflows.client.WorkflowInstanceStatus;
-import io.dapr.workflows.runtime.WorkflowRuntimeBuilder;
-import io.quarkus.logging.Log;
-
-@Path("/workflows/simple")
+@Path("/workflows")
 public class SimpleResource {
 
     @Inject
@@ -22,6 +24,7 @@ public class SimpleResource {
     @Inject
     DaprWorkflowClient daprWorkflowClient;
 
+    @Path("/uppercase")
     @POST
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
@@ -31,10 +34,27 @@ public class SimpleResource {
 
         Log.info("Starting SimpleWorkflow with instance ID as " + instanceId);
 
-        Thread.sleep(4000);
-
-        WorkflowInstanceStatus instanceState = daprWorkflowClient.getInstanceState(instanceId, true);
-
-        return Response.ok().entity(instanceState.getSerializedOutput()).build();
+        return Response.accepted()
+                .header("X-Instance-Id", instanceId)
+                .build();
     }
-}
+
+    @GET
+    @Path("/{workflowId}/result")
+    public Response workflowId(@PathParam("workflowId") String workflowId) {
+
+        WorkflowInstanceStatus state = daprWorkflowClient.getInstanceState(workflowId, true);
+        assert state != null;
+
+        if (state.getRuntimeStatus().equals(WorkflowRuntimeStatus.COMPLETED)) {
+            Log.info("Workflow completed successfully");
+            return  Response.ok(
+                    state.getSerializedOutput()
+            ).build();
+        }
+
+        Log.info("Current status " + state.getRuntimeStatus());
+
+        return Response.ok(state.readOutputAs(String.class)).build();
+    }
+ }
