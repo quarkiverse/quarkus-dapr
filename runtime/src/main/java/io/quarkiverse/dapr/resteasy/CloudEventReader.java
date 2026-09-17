4eb1a52f277f010761ec7fd229baf3fb94a92503
Matheus André <92062874+matheusandre1@users.noreply.github.com>
2026-09-17 17:27:43 -0300

    refactor: support for other datacontenttypes (#402)
    * refactor: support for other datacontenttypes
    
    * test: add CloudEvent text/plain handling in DaprResourceTest

--- runtime/src/main/java/io/quarkiverse/dapr/resteasy/CloudEventReader.java
+++ runtime/src/main/java/io/quarkiverse/dapr/resteasy/CloudEventReader.java
@@ -10,13 +10,15 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 import jakarta.enterprise.inject.spi.CDI;
-import jakarta.ws.rs.NotSupportedException;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.MultivaluedMap;
-import jakarta.ws.rs.ext.MessageBodyReader;
 import jakarta.ws.rs.ext.Provider;
+
+import org.jboss.resteasy.reactive.server.spi.ResteasyReactiveResourceInfo;
+import org.jboss.resteasy.reactive.server.spi.ServerMessageBodyReader;
+import org.jboss.resteasy.reactive.server.spi.ServerRequestContext;
 
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -27,13 +29,18 @@ import io.quarkiverse.dapr.config.DaprConfig;
 
 /**
  * CloudEventReader
+ * <p>
+ * Implements {@link ServerMessageBodyReader} so that RESTEasy Reactive does not need to reflectively look up the
+ * resource method to obtain parameter annotations. A plain {@code MessageBodyReader} triggers
+ * {@code Class.getMethod(...)} at request time, which fails in native mode because resource methods that only take a
+ * {@code CloudEvent} parameter are not registered for reflection.
  *
  * @author naah69
  * @date 2022/4/25 10:04 AM
  */
 @Provider
 @Produces(CloudEvent.CONTENT_TYPE)
-public class CloudEventReader implements MessageBodyReader<CloudEvent> {
+public class CloudEventReader implements ServerMessageBodyReader<CloudEvent> {
 
     private static ObjectMapper OBJECT_MAPPER;
     private static DaprConfig DAPR_CONFIG;
@@ -49,44 +56,66 @@ public class CloudEventReader implements MessageBodyReader<CloudEvent> {
     }
 
     @Override
+    public boolean isReadable(Class<?> type, Type genericType, ResteasyReactiveResourceInfo lazyMethod,
+            MediaType mediaType) {
+        return type == CloudEvent.class;
+    }
+
+    @Override
     public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
         return type == CloudEvent.class;
+    }
+
+    @Override
+    public CloudEvent readFrom(Class<CloudEvent> type, Type genericType, MediaType mediaType,
+            ServerRequestContext context) throws WebApplicationException, IOException {
+        return read(genericType, context.getInputStream());
     }
 
     @Override
     public CloudEvent readFrom(Class<CloudEvent> type, Type genericType, Annotation[] annotations, MediaType mediaType,
             MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
             throws IOException, WebApplicationException {
+        return read(genericType, entityStream);
+    }
+
+    private static CloudEvent read(Type genericType, InputStream entityStream) throws IOException {
         JavaType valueType = TYPE_CACHE.computeIfAbsent(genericType,
                 a -> OBJECT_MAPPER.getTypeFactory().constructType(genericType));
         JsonNode jsonNode = OBJECT_MAPPER.readTree(entityStream);
         return getCloudEvent(jsonNode, valueType);
-
     }
 
     private static CloudEvent getCloudEvent(JsonNode jsonNode, JavaType valueType)
             throws IOException {
-        String dataContentType = jsonNode.get("datacontenttype").asText();
-        switch (dataContentType) {
-            case MediaType.APPLICATION_JSON:
-                return OBJECT_MAPPER.treeToValue(jsonNode, valueType);
-            case MediaType.TEXT_PLAIN:
-                String data = jsonNode.get("data").asText();
-                return OBJECT_MAPPER.readValue(data, valueType);
-            case MediaType.APPLICATION_OCTET_STREAM:
-                byte[] binaryData = jsonNode.get("data_base64").binaryValue();
-                String pubsubname = jsonNode.get("pubsubname").asText();
-                String rawPayload = Optional.ofNullable(DAPR_CONFIG.pubSub().get(pubsubname))
-                        .map(a -> a.consumeMetadata())
-                        .map(a -> a.get("rawPayload"))
-                        .orElse("");
-                if (Objects.equals("true", rawPayload)) {
-                    JsonNode subJsonNode = OBJECT_MAPPER.readTree(binaryData);
-                    return getCloudEvent(subJsonNode, valueType);
-                }
-                return OBJECT_MAPPER.readValue(binaryData, valueType);
-            default:
-                throw new NotSupportedException("can't read unknown cloud event content type: " + dataContentType);
+        String dataContentType = Optional.ofNullable(jsonNode.get("datacontenttype"))
+                .map(JsonNode::asText)
+                .orElse(MediaType.APPLICATION_JSON);
+        if (isOctetStream(dataContentType)) {
+            byte[] binaryData = jsonNode.get("data_base64").binaryValue();
+            String pubsubname = jsonNode.get("pubsubname").asText();
+            String rawPayload = Optional.ofNullable(DAPR_CONFIG.pubSub().get(pubsubname))
+                    .map(a -> a.consumeMetadata())
+                    .map(a -> a.get("rawPayload"))
+                    .orElse("");
+            if (Objects.equals("true", rawPayload)) {
+                JsonNode subJsonNode = OBJECT_MAPPER.readTree(binaryData);
+                return getCloudEvent(subJsonNode, valueType);
+            }
+            return OBJECT_MAPPER.readValue(binaryData, valueType);
+        }
+        return OBJECT_MAPPER.treeToValue(jsonNode, valueType);
+    }
+
+    private static boolean isOctetStream(String dataContentType) {
+        if (dataContentType == null) {
+            return false;
+        }
+        try {
+            MediaType mediaType = MediaType.valueOf(dataContentType);
+            return mediaType.isCompatible(MediaType.APPLICATION_OCTET_STREAM_TYPE);
+        } catch (IllegalArgumentException ex) {
+            return dataContentType.startsWith(MediaType.APPLICATION_OCTET_STREAM);
         }
     }
 
